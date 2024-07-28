46cf143d76107357a94753b261003cd6a7ae4539
Pasquale Giaccio <pasquale.giaccio1998@gmail.com>
2024-07-28 19:08:01 +0200

    Fix to correctly load new levels from main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -8,7 +8,9 @@ func _on_new_game_button_pressed():
 	if not first_level:
 		printerr("unable to start new game, no level")
 		return
-	get_tree().change_scene_to_file(first_level.resource_path)
+	
+	SceneManager.goto_scene(first_level.resource_path)
+
 
 func _on_exit_button_pressed():
 	get_tree().quit()
