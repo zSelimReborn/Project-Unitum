79d9649e917a87a51a49b36f4cba6183f47ea9e4
Pasquale Giaccio <pasquale.giaccio1998@gmail.com>
2024-07-29 22:50:27 +0200

    Credits in main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,7 +2,12 @@ class_name MainMenu
 
 extends Control
 
+# Properties
 @export var first_level : PackedScene
+
+# On Ready
+@onready var main_container = $Panel/MainContainer
+@onready var credits_container = $Panel/CreditsContainer
 
 func _on_new_game_button_pressed():
 	if not first_level:
@@ -14,3 +19,13 @@ func _on_new_game_button_pressed():
 
 func _on_exit_button_pressed():
 	get_tree().quit()
+
+
+func _on_credits_button_pressed():
+	main_container.hide()
+	credits_container.show()
+
+
+func _on_back_button_pressed():
+	main_container.show()
+	credits_container.hide()
